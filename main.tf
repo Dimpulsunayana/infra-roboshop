95b9b4b88c759d949ac2a08082d84ad7b69ee8e2
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 16:07:42 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "github.com/Dimpulsunayana/tf-vpc-subnets"
 
   env =var.env
-  cidr_block = var.cidr_block
+ # cidr_block = var.cidr_block
   default_vpc_id = var.default_vpc_id
 
   for_each = var.vpc
