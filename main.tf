821887f5e310023aa7a37b3e3b9dcd155dc2f8bf
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 16:16:58 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   for_each = var.vpc
  public_subnets = each.value.public_subnets
 #  private_subnets = each.value.private_subnets
-#  availability_zone = each.value.availability_zone
-#  cidr_block = each.value.cidr_block
-#  name = each.value.name
+  availability_zone = each.value.availability_zone
+  cidr_block = each.value.cidr_block
+  name = each.value.name
 }
