48f79edffa05370e4e07fc7d0279f2a8543dfb2b
windows 10 <dimpulsunayana205@gmail.com>
2023-04-01 11:24:02 +0530

    updated user repo

--- var.tf
+++ var.tf
@@ -4,5 +4,5 @@ variable "default_vpc_id" {}
 variable "vpc" {}
 #variable "main_vpc" {}
 variable "docdb" {}
-variable "instance_class" {}
-variable "engine_version" {}
+#variable "instance_class" {}
+#variable "engine_version" {}
