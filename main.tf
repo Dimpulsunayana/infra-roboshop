6979df9949dacfb3365a982d88d2721dd0c05af1
windows 10 <dimpulsunayana205@gmail.com>
2023-04-12 16:09:51 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -29,52 +29,52 @@ module "DOCDB" {
   engine_version = each.value.engine_version
 }
 
-#module "rds" {
-#  source = "github.com/Dimpulsunayana/rds_tf"
-#  env = var.env
-#  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
-#  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
-#  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
-#
-#  for_each = var.rds
-#  instance_class = each.value.instance_class
-#  engine_version = each.value.engine_version
-#  engine = each.value.engine
-#
-#
+module "rds" {
+  source = "github.com/Dimpulsunayana/rds_tf"
+  env = var.env
+  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
+  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
+
+  for_each = var.rds
+  instance_class = each.value.instance_class
+  engine_version = each.value.engine_version
+  engine = each.value.engine
+
+
+}
+
+#output "rs" {
+#  value = module.rds
 #}
-#
-##output "rs" {
-##  value = module.rds
-##}
-#
-#module "elasticache" {
-#  source = "github.com/Dimpulsunayana/elasticache_tf"
-#  env = var.env
-#  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
-#  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
-#  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
-#
-#  for_each = var.redis
-#  node_type = each.value.node_type
-#  #engine_version = each.value.engine_version
-#  //vpc_name = each.value.vpc_name
-#}
-#
-#module "rabbitmq" {
-#  source = "github.com/Dimpulsunayana/rabbitmq-tf"
-#  env = var.env
-#  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
-#  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
-#  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
-#
-#  for_each = var.rabbitmq
-#  engine_type = each.value.engine_type
-#  engine_version = each.value.engine_version
-#  host_instance_type = each.value.host_instance_type
-#  deployment_mode = each.value.deployment_mode
-#}
-#
+
+module "elasticache" {
+  source = "github.com/Dimpulsunayana/elasticache_tf"
+  env = var.env
+  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
+  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
+
+  for_each = var.redis
+  node_type = each.value.node_type
+  #engine_version = each.value.engine_version
+  //vpc_name = each.value.vpc_name
+}
+
+module "rabbitmq" {
+  source = "github.com/Dimpulsunayana/rabbitmq-tf"
+  env = var.env
+  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets", null), "private", null), "subnet_ids", null)
+  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  main_vpc = lookup(lookup(module.vpc, "main",null ),"main_vpc" , null)
+
+  for_each = var.rabbitmq
+  engine_type = each.value.engine_type
+  engine_version = each.value.engine_version
+  host_instance_type = each.value.host_instance_type
+  deployment_mode = each.value.deployment_mode
+}
+
 module "alb-tf" {
   source = "github.com/Dimpulsunayana/alb-tf"
   env = var.env
